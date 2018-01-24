d638b77f16fd0d1e35f85fcfbea1757f1d623aee
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2018-01-24 14:40:03 -0800

    Configured Sequelize

---
+++ migrations/20180124222913-create-pet.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Pets', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id: {
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      species: {
+        type: Sequelize.STRING
+      },
+      birthday: {
+        type: Sequelize.STRING
+      },
+      favoriteFood: {
+        type: Sequelize.STRING
+      },
+      picUrl: {
+        type: Sequelize.TEXT
+      },
+      picUrlSq: {
+        type: Sequelize.TEXT
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Pets');
+  }
+};
---
+++ models/pet.js
@@ -0,0 +1,20 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Pet = sequelize.define('Pet', {
+    id: DataTypes.INTEGER,
+    name: DataTypes.STRING,
+    species: DataTypes.STRING,
+    birthday: DataTypes.STRING,
+    favoriteFood: DataTypes.STRING,
+    picUrl: DataTypes.TEXT,
+    picUrlSq: DataTypes.TEXT,
+    description: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Pet;
+};
